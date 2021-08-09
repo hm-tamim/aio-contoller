38f7040f13c3351d0bb60be915138bcd7fce534b
HM Tamim <hm.tamim@evaly.com.bd>
2021-08-09 21:31:09 +0600

    AIO controller for Kraken X52

---
+++ FanConfig.bat
@@ -0,0 +1,4 @@
+liquidctl --match kraken set fan speed  20 30  30 50  34 55  40 60  50 70 60 80 70 100
+liquidctl --match kraken set pump speed 90
+liquidctl --match kraken set sync color spectrum-wave
+liquidctl --match kraken set logo color fixed 247fe6
